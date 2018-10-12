3fc81acbd3265c44ad2ab133e74b81f8daa7706b
Dominic Gomez <nic@maxwell.socal.rr.com>
2018-10-11 23:31:21 -0700

    Added tests for decks with 3 stddecks.

--- src/decktest.c
+++ src/decktest.c
@@ -5,11 +5,10 @@
 
 int main()
 {
-    printf("Creating empty deck...");
+    printf("Creating deck with 1 set of standard playing cards...");
     deck *d = deck_init(1);
     assert(d);
     printf("DONE\n");
-
     printf("Verifying appropriate top position in deck...");
     assert(d->top == 0);
     printf("DONE\n");
@@ -19,7 +18,6 @@ int main()
     printf("Verifying deck not shuffled...");
     assert(!d->shuffled);
     printf("DONE\n");
-
     printf("Verifying deck not ready for use in play in a new game...");
     assert(!deck_good(d));
     printf("DONE\n");
@@ -32,10 +30,8 @@ int main()
     printf("Verifying deck not shuffled...");
     assert(!deck_shuffled(d));
     printf("DONE\n");
-
     printf("Unshuffled deck: ");
     deck_print(d);
-
     printf("Preparing deck for use in play in a new game...");
     deck_prep(d);
     printf("DONE\n");
@@ -51,10 +47,8 @@ int main()
     printf("Verifying deck shuffled...");
     assert(deck_shuffled(d));
     printf("DONE\n");
-
     printf("Shuffled deck: ");
     deck_print(d);
-
     printf("Drawing card from deck...");
     card c = deck_draw(d);
     printf("DONE\n");
@@ -62,7 +56,6 @@ int main()
     card_println(&c);
     printf("Deck: ");
     deck_print(d);
-
     printf("Drawing card from deck...");
     c = deck_draw(d);
     printf("DONE\n");
@@ -70,7 +63,6 @@ int main()
     card_println(&c);
     printf("Deck: ");
     deck_print(d);
-
     printf("Drawing card from deck...");
     c = deck_draw(d);
     printf("DONE\n");
@@ -78,7 +70,6 @@ int main()
     card_println(&c);
     printf("Deck: ");
     deck_print(d);
-
     printf("Counting remaining cards in deck...");
     int r = deck_remaining(d);
     printf("DONE\n");
@@ -92,7 +83,6 @@ int main()
     r = deck_remaining(d);
     printf("DONE\n");
     printf("Remaining cards: %d\n", r);
-
     printf("Verifying deck not ready for use in play in a new game...");
     assert(!deck_good(d));
     printf("DONE\n");
@@ -102,12 +92,100 @@ int main()
     printf("Verifying deck empty...");
     assert(deck_empty(d));
     printf("DONE\n");
-
     printf("Killing deck...");
     deck_kill(d);
     printf("DONE\n");
-    // TODO: Why is this still a valid memory address if I set it to null in the destructor?
-    /* printf("%p\n", (void *)d); */
+
+    printf("Creating deck with 3 sets of standard playing cards...");
+    d = deck_init(3);
+    assert(d);
+    printf("DONE\n");
+    printf("Verifying appropriate top position in deck...");
+    assert(d->top == 0);
+    printf("DONE\n");
+    printf("Verifying appropriate number of sets in deck...");
+    assert(d->stddecks == 3);
+    printf("DONE\n");
+    printf("Verifying deck not shuffled...");
+    assert(!d->shuffled);
+    printf("DONE\n");
+    printf("Verifying deck not ready for use in play in a new game...");
+    assert(!deck_good(d));
+    printf("DONE\n");
+    printf("Verifying deck full...");
+    assert(deck_full(d));
+    printf("DONE\n");
+    printf("Verifying deck not empty...");
+    assert(!deck_empty(d));
+    printf("DONE\n");
+    printf("Verifying deck not shuffled...");
+    assert(!deck_shuffled(d));
+    printf("DONE\n");
+    printf("Unshuffled deck: ");
+    deck_print(d);
+    printf("Preparing deck for use in play in a new game...");
+    deck_prep(d);
+    printf("DONE\n");
+    printf("Verifying deck ready for use in play in a new game...");
+    assert(deck_good(d));
+    printf("DONE\n");
+    printf("Verifying deck full...");
+    assert(deck_full(d));
+    printf("DONE\n");
+    printf("Verifying deck not empty...");
+    assert(!deck_empty(d));
+    printf("DONE\n");
+    printf("Verifying deck shuffled...");
+    assert(deck_shuffled(d));
+    printf("DONE\n");
+    printf("Shuffled deck: ");
+    deck_print(d);
+    printf("Drawing card from deck...");
+    c = deck_draw(d);
+    printf("DONE\n");
+    printf("Drawn card: ");
+    card_println(&c);
+    printf("Deck: ");
+    deck_print(d);
+    printf("Drawing card from deck...");
+    c = deck_draw(d);
+    printf("DONE\n");
+    printf("Drawn card: ");
+    card_println(&c);
+    printf("Deck: ");
+    deck_print(d);
+    printf("Drawing card from deck...");
+    c = deck_draw(d);
+    printf("DONE\n");
+    printf("Drawn card: ");
+    card_println(&c);
+    printf("Deck: ");
+    deck_print(d);
+    printf("Counting remaining cards in deck...");
+    r = deck_remaining(d);
+    printf("DONE\n");
+    printf("Remaining cards: %d\n", r);
+    printf("Drawing all remaining cards from deck...");
+    for (int i = 0; i < r; ++i) {
+        deck_draw(d);
+    }
+    printf("DONE\n");
+    printf("Counting remaining cards in deck...");
+    r = deck_remaining(d);
+    printf("DONE\n");
+    printf("Remaining cards: %d\n", r);
+    printf("Verifying deck not ready for use in play in a new game...");
+    assert(!deck_good(d));
+    printf("DONE\n");
+    printf("Verifying deck not full...");
+    assert(!deck_full(d));
+    printf("DONE\n");
+    printf("Verifying deck empty...");
+    assert(deck_empty(d));
+    printf("DONE\n");
+    printf("Killing deck...");
+    deck_kill(d);
+    printf("DONE\n");
 
     return EXIT_SUCCESS;
 }
